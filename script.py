34833308a0c7181d5ebf95c4e20903796a7334d2
Sanjeev Bhusal <bhusalsanjeev23@gmail.com>
2022-10-16 16:54:50 +0545

    added to a file

--- script.py
+++ script.py
@@ -1,16 +1,26 @@
 with open("data.txt", "r") as f:
     data_set = f.readlines()
     state_detail_list = []
+    uncommon_length = [1, 2, 3]
 
     for data in data_set:
-        two_state_detail = data.split("    ")
-        first_state_detail = two_state_detail[:3]
-        state_detail_list.append(first_state_detail)
+        data_split = data.split("    ")
+        length_of_data = len(data_split)
 
-        second_state_detail = two_state_detail[3:]
-        state_detail_list.append(second_state_detail) if len(second_state_detail) > 3 else None
+        if length_of_data in uncommon_length:
+            state_detail_list.append([detail.strip() for detail in data_split])
+        else:
+            first_state_detail = data_split[:3]
+            second_state_detail = data_split[3:]
+            state_detail_list.append([detail.strip() for detail in first_state_detail])
+            state_detail_list.append([detail.strip() for detail in second_state_detail])
 
-print(state_detail_list)
+
+with open("state_detail.txt", "w") as f:
+    f.write(str(state_detail_list))
+
+
+
 
 
 
