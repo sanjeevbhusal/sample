e017c51769e9005be80d37377e7d082afe7faf79
Sanjeev Bhusal <bhusalsanjeev23@gmail.com>
2022-10-16 18:24:51 +0545

    fixed

--- script.py
+++ script.py
@@ -1,41 +1,57 @@
-def merge_details(state_info, key_list):
-    state_detail = dict.fromkeys(key_list)
-    for index, detail in enumerate(state_info):
-        detail = detail.strip()
-        if index == 0:
-            state_detail["State"] = detail
-        elif index == 1:
-            state_detail["Postal Abbr."] = detail
-        else:
-            state_detail["FIPS Code"] = int(detail)
+def get_file_headers(f):
+    header_full_name = {"Postal": "Postal Abbr", "FIPS": "FIPS Code"}
+    headers_with_line_ending = list(header_full_name)
+    headers_to_ignore = ["Abbr.", "Code"]
+    cluttered_file_header_list = [f.readline() for _ in range(5)]
+    header_list = []
+
+    for headers_group_string in cluttered_file_header_list:
+        headers_group__list = headers_group_string.split()
+        for header in headers_group__list:
+            header = header.strip()
+            if header in headers_to_ignore or header in header_list:
+                continue
+            elif header in headers_with_line_ending:
+                header = header_full_name[header]
+                if header not in header_list:
+                    header_list.append(header)
+            else:
+                header_list.append(header)
+
+    return header_list
+
+
+def merge_details(data, header_list):
+    state_detail = {}
+    for index, header in enumerate(header_list):
+        state_detail[header] = data[index].strip()
     return state_detail
 
 
 with open("data.txt", "r") as f:
-    key_list = [key.strip() for key in f.readline().split("    ")]
-    data_set = f.readlines()
+    file_header_list = get_file_headers(f)
+    file_data = f.readlines()
     state_detail_list = []
 
-    for data in data_set:
+    for data in file_data:
         data_split = data.split("    ")
         length_of_data = len(data_split)
 
         if length_of_data == 6:
             first_state_detail = data_split[:3]
             second_state_detail = data_split[3:]
-            state_detail_list.append(merge_details(first_state_detail, key_list))
-            state_detail_list.append(merge_details(second_state_detail, key_list))
+            state_detail_list.append(merge_details(first_state_detail, file_header_list))
+            state_detail_list.append(merge_details(second_state_detail, file_header_list))
         else:
-            state_detail_list.append(merge_details(data_split, key_list))
+            state_detail_list.append(merge_details(data_split, file_header_list))
+
 
 with open("state_detail.txt", "w") as f:
     f.write(str(state_detail_list))
-#
-#
-#
-#
-#
 
 
-# print(dict.fromkeys([('Java', 14), ('Python', 3), ('JavaScript', 6)]))
-# print(dict.fromkeys(["state", "postal", "id"]))
+
+
+
+
+
