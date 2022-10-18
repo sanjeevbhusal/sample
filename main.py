d565a00ed423a266a6f5dd74d40865f17bfba7f8
Sanjeev Bhusal <bhusalsanjeev23@gmail.com>
2022-10-18 12:51:04 +0545

    formatted again

--- main.py
+++ main.py
@@ -20,14 +20,9 @@ for note in note_list:
         two_ngram_words_dict[word] += 1
     for word in three_ngram:
         three_ngram_words_dict[word] += 1
-
-
-top_20_two_ngram = dict(itertools.islice(
-    {key: value for key, value in sorted(two_ngram_words_dict.items(), key=lambda x: x[1], reverse=True)}.items(), 20))
-top_20_three_ngram = dict(itertools.islice(
-    {key: value for key, value in sorted(three_ngram_words_dict.items(), key=lambda x: x[1], reverse=True)}.items(), 20))
-
+top_20_two_ngram = dict(sorted(two_ngram_words_dict.items(), key=lambda x: x[1], reverse=True)[:20])
+top_20_three_ngram = dict(sorted(three_ngram_words_dict.items(), key=lambda x: x[1], reverse=True)[:20])
 
 with open("ngrams.json", "w") as f:
-    json.dump({"top_20_two_ngram": top_20_two_ngram, "top_20_three_ngram": top_20_three_ngram}, f)
+    json.dump({"top_20_two_ngram": top_20_two_ngram, "top_20_three_ngram": top_20_three_ngram}, f, indent=4)
 
