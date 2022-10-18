d8f4386b273b2a602d34c6cb8ec6f8e7b6f7caef
Sanjeev Bhusal <bhusalsanjeev23@gmail.com>
2022-10-18 12:10:20 +0545

    top 20 ngram created

--- main.py
+++ main.py
@@ -3,19 +3,26 @@ from calculate_ngram import get_ngrams
 from collections import defaultdict
 
 note_list = []
-words_dict = defaultdict(int)
+two_ngram_words_dict = defaultdict(int)
+three_ngram_words_dict = defaultdict(int)
 
 with open("govt_url.csv", "r") as f:
     reader = csv.DictReader(f)
     for row in reader:
         note_list.append(row["Note"])
 
-
 for note in note_list:
     two_ngram = get_ngrams(note, 2)
     three_ngram = get_ngrams(note, 3)
-    word_list = two_ngram + three_ngram
-    for word in word_list:
-        words_dict[word] += 1
+    for word in two_ngram:
+        two_ngram_words_dict[word] += 1
+    for word in three_ngram:
+        three_ngram_words_dict[word] += 1
 
+top_20_two_ngram = [key for key, value in sorted(two_ngram_words_dict.items(), key=lambda x: x[1], reverse=True)][:20]
+top_20_three_ngram = [key for key, value in sorted(three_ngram_words_dict.items(), key=lambda x: x[1], reverse=True)][:20]
 
+with open("top_20.txt", "w") as f:
+    f.write(str({"top_20_two_ngram": top_20_two_ngram}))
+    f.write("\n")
+    f.write(str({"top_20_three_ngram": top_20_three_ngram}))
